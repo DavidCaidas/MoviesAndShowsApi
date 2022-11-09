471e8661a522aca6f286ec1a4b93f59b00cfaa64
David <104197044+DavidCaidas@users.noreply.github.com>
2022-11-09 16:24:47 +0100

    created remote Api classes

---
+++ app/src/main/java/com/example/movies_and_shows_api/data/remote/api/ApiModels.kt
@@ -0,0 +1,13 @@
+package com.example.movies_and_shows_api.data.remote.api
+
+
+data class moviesApiModel(
+    val id: String,
+    val title: String,
+    val plot: String,
+    val genre: String,
+    val rating: String,
+    val year: String,
+    val poster: String
+
+)
