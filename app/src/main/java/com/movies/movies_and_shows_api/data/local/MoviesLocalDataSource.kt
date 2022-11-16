dfbac213bede402b2b92bb5c46d85782e937a682
David <104197044+DavidCaidas@users.noreply.github.com>
2022-11-16 18:26:07 +0100

    CreatedLocalSource xml and memory

---
+++ app/src/main/java/com/movies/movies_and_shows_api/data/local/MoviesLocalDataSource.kt
@@ -0,0 +1,9 @@
+package com.movies.movies_and_shows_api.data.local
+
+import com.movies.movies_and_shows_api.domain.Movie
+
+interface MoviesLocalDataSource {
+    fun getAllMovies():List<Movie>
+    fun getMovieById(id:String):Movie?
+    fun save(list:List<Movie>)
+}
